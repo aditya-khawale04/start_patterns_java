dac8b0c07e4c2a50c015e186709551d3b8727154
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 12:35:17 +0530

    Create PloydsTriangle.java

---
+++ PloydsTriangle.java
@@ -0,0 +1,13 @@
+class PloydsTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+        int counter =1;
+        for(int i = 1; i <= rows; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
