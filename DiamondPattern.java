f481a1fdec76eea3d03f27e4756d14f67752892e
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 19:12:48 +0530

    Simplify star printing in diamond pattern
    Refactored diamond pattern logic to simplify star printing.

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -5,10 +5,7 @@ class DiamondPattern {
             for(int j = 1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int j = 1; j<i; j++){
-                System.out.print("*");
-            }
-            for(int j = 2; j<i; j++){
+            for(int j = 1; j<2*i-2; j++){
                 System.out.print("*");
             }
             System.out.println();
@@ -17,10 +14,7 @@ class DiamondPattern {
             for(int j = 1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int j = 1; j<i; j++){
-                System.out.print("*");
-            }
-            for(int j = 2; j<i; j++){
+            for(int j = 1; j<2*i-2; j++){
                 System.out.print("*");
             }
             System.out.println();
