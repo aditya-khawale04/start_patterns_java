6af322b6484088f76f7fd1a56413f65a64d027ed
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 13:01:05 +0530

    Add Triangle01.java to print a triangle pattern

---
+++ Triangle01.java
@@ -0,0 +1,16 @@
+class Triangle01 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                if((j+i) % 2 == 0){
+                    System.out.print(1 + " ");
+                }else{
+                    System.out.print(0 + " ");
+                }
+            
+            }
+            System.out.println();
+        }
+    }
+}
