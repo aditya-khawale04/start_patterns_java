8767fef1f4fa67818a342b6ddb0bf9341cde3645
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 11:35:11 +0530

    Update hollow rectangle logic to include borders
    Modified the condition to print '*' for the hollow rectangle to include the first and last columns.

--- hollowRectangle.java
+++ hollowRectangle.java
@@ -4,9 +4,7 @@ class Main {
         int cols = 5;
         for(int i = 1; i<=rows; i++){
             for(int j = 1; j<=cols; j++){
-                if(i== 1 || i==rows){
-                    System.out.print("*");
-                }else if(j == 1 || j==cols){
+                if(i== 1 || i==rows || j == 1 || j==cols){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
