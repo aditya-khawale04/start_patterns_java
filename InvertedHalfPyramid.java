072b382458e561c1f161cbaa909daf03815b3624
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 11:46:55 +0530

    Fix loop condition for inverted half pyramid

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -1,7 +1,7 @@
 class InvertedHalfPyramid {
     public static void main(String[] args) {
         int rows = 4;
-        for(int i = 4; i<=rows; i--){
+        for(int i = rows; i>=1; i--){
             for(int j = 1; j<=i; j++){
                 System.out.print("*");
             }
