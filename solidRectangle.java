aff6e6d9d2582d8cead811c728b65e2ba717ace5
Aditya Khawale <155005852+aditya-khawale04@users.noreply.github.com>
2026-07-18 11:31:52 +0530

    Add solid rectangle printing in solidRectangle.java
    Implement a program to print a solid rectangle of asterisks.

---
+++ solidRectangle.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int rows = 3;
+        int cols = 4;
+        for(int i = 1; i<=rows; i++){
+            for(int j = 1; j<=cols; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// ****
+// ****
+// ****
